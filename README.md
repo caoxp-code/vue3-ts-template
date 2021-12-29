e62d6a79466ce3cf23c2734e2e12a7fd6027ce02
xiaopeng <xiaopeng@velo.org>
2021-12-29 15:09:11 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vue3-ts-template
-集成vue3+ts+tailwind的模板
+创建一个常使用的vue3模板，其中包括typescript，tailwind. 以及eslint，stylelint，husky等一些开发流程梳理的组件。
+优化开发体验，减少初始化项目难度。
+## engine
+node >= 16
---
+++ src/router/index.ts
@@ -0,0 +1,6 @@
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+const routes: RouteRecordRaw[] = []
+export default createRouter({
+    history: createWebHistory(),
+    routes
+})
